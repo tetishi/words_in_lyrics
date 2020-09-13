b96b59d9523c2aa13dae4f5b3ebef10513f2dbdc
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-09-13 15:52:52 +0900

    Try to display search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,38 +2,46 @@
 
 class SearchesController < ApplicationController
   def index
-  end
-
-  def show
     # どの単語でも
-    # words = params[:q]
-    # search_words = "q=#{words}"
+    words = params[:query]
+    search_words = "q=#{words}"
 
-    params = URI.encode_www_form({search: "q=Kendrick%20Lamar"})
-    token = "OWOAJA6tyCbNQKQBi3NUZkkY_xdtdZX3ZNTC-mXb_4g3Gwz7gGjqPprtaUs7VZNB"
-    uri = URI.parse("https://api.genius.com/search?#{params}")
+    # params = URI.encode_www_form({search: search_words})
+    # params = URI.encode_www_form({search: "q=Kendrick%20Lamar"})
+    token = ENV["CLIENT_ACCESS_TOKEN"]
+    uri = URI.parse("https://api.genius.com/search?#{search_words}")
     @query = uri.query
+    http = Net::HTTP.new(uri.host, uri.port)
 
-    response = Net::HTTP.start(uri.host, uri.port) do |http|
-      http.open_timeout = 5
-      http.read_timeout = 10
-      http.get(uri.request_uri)
-    end
-　　# ここは動いてる
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    # response = Net::HTTP.start(uri.host, uri.port) do |http|
+    #   http.open_timeout = 5
+    #   http.read_timeout = 10
+    #   http.get(uri.request_uri)
+    # end
+    # ここは動いてる
+
+    # binding.pry
 
     req = Net::HTTP::Get.new(uri.request_uri)
     req["Authorization"] = "Bearer #{token}"
-    
+
     # Authorizationになにも入っていないから入れる
+    # binding.pry
 
-    req = http.request(req)
+    response = http.request(req)
+    # api_response = JSON.parse(res.body)
 
-    binding.irb
+    # binding.pry
 
+    # このとってきかたに問題がある@resultとか全部hashなのでhashを勉強し直す
     begin
       case response
       when Net::HTTPSuccess
         @result = JSON.parse(response.body)
+        # 全部nilだからデータのとってきかたに問題がある
         @search = @result["results"][0]["search"]
         @full_title = @result["results"][0]["full_title"]
         @header_image_thumbnail_url = @result["results"][0]["@header_image_thumbnail_url"]
@@ -46,8 +54,10 @@ class SearchesController < ApplicationController
         @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
       end
     rescue => exception
-      
+      @message = "e.message"
     end
+
+    binding.pry
     # query = params[:query]
     # return unless query.present?
 
@@ -55,7 +65,11 @@ class SearchesController < ApplicationController
     # sounds = spotify.search(query)
     # @results = formatted(sounds)
   end
-end
+  end
+
+  def show
+    
+  end
 
 # private
 #   def formatted(response)
